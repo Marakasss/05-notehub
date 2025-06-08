12eb775272b7a049e2e6ffa8f7732d44c610b65a
Marakas <igorpetriv.bsc@gmail.com>
2025-06-08 03:07:06 +0300

    m-n add

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App.tsx";
+import "modern-normalize";
 
 const queryClient = new QueryClient();
 
